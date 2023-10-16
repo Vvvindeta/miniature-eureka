77cf2ccdfec4a3788eeeadf247153f64b760db27
Hogen <dimal2003play@gmail.com>
2023-10-16 18:32:05 +0300

    second

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,90 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
 
-#include <iostream>
+class Base {
+public:
+    virtual void serialize(std::ostream& os) const = 0;
+    virtual void deserialize(std::istream& is) = 0;
+    virtual ~Base() = default;
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Derived : public Base {
+public:
+    int data;
+
+    void serialize(std::ostream& os) const override {
+        os << data << " ";
+    }
+
+    void deserialize(std::istream& is) override {
+        is >> data;
+    }
+};
+
+class Keeper {
+private:
+    static const int maxObjects = 100;
+    Base* objects[maxObjects];
+    int objectCount;
+
+public:
+    Keeper() : objectCount(0) {}
+
+    void addObject(Base* obj) {
+        if (objectCount < maxObjects) {
+            objects[objectCount] = obj;
+            objectCount++;
+        }
+    }
+
+    void removeObject(int index) {
+        if (index >= 0 && index < objectCount) {
+            delete objects[index];
+            for (int i = index; i < objectCount - 1; i++) {
+                objects[i] = objects[i + 1];
+            }
+            objectCount--;
+        }
+    }
+
+    void saveToFile(const std::string& filename) const {
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            for (int i = 0; i < objectCount; i++) {
+                objects[i]->serialize(file);
+                file << std::endl;  
+            }
+        }
+    }
+
+    void loadFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            objectCount = 0;
+            while (!file.eof()) {
+
+                Base* obj = new Derived;
+                obj->deserialize(file);
+                addObject(obj);
+            }
+        }
+    }
+};
+
+int main() {
+    Keeper keeper;
+
+    Derived* obj1 = new Derived;
+    Derived* obj2 = new Derived;
+    obj1->data = 42;
+    obj2->data = 123;
+    keeper.addObject(obj1);
+    keeper.addObject(obj2);
+
+    keeper.saveToFile("objects.txt");
+
+    Keeper loadedKeeper;
+    loadedKeeper.loadFromFile("objects.txt");
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
